6f6dc30d39e37ed0deec0736626081c86d5d7f48
Matthew Slanaker <matt.slanaker@gmail.com>
2021-10-08 15:32:41 -0400

    set up server connection to Mongo DB

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv = require ("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(console.log("Now connected to Mongodb"))
+.catch((err) => console.log(err));
+
+
+app.listen("5000", () => {
+    console.log("Backend is running.");
+});
+
