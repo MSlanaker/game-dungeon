86aeccadebe8443082071db2690a61899df025d2
jcorozco81 <jcorozco@gmail.com>
2021-10-09 22:54:29 -0400

    Fixed functions and made the exportable

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,87 +1,101 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_CLIENT_ID="";
-const API_SECRET="";
-const API_AUTH="";
+// const { API_CLIENT_ID, API_SECRET } = process.env;
+
+// Need to fill
+const API_CLIENT_ID = "";
+const API_SECRET = "";
+const API_AUTH = "";
 
 const authURL = `https://id.twitch.tv/oauth2/token?client_id=${API_CLIENT_ID}&client_secret=${API_SECRET}&grant_type=client_credentials`;
+const searchURL = "https://api.igdb.com/v4/search";
+const imgURL = "https://api.igdb.com/v4/screenshots";
+const dataURL = "https://api.igdb.com/v4/games/";
+// const gameID = 1942;
+// const searchText = `Call of Duty`;
+
+const API = {
+  async newAuth() {
+  // const APInewAuth = await
+  let res;
+  try{
+  res = await axios({
+      method: 'POST',
+      url: authURL});
+      console.log(res.data);
+  }catch (err) {
+      console.log(err)
+    }
+  return res.data;
+  },
 
 
 
-
-
-
-export const APInewAuth = () => {
-    axios({
-    method: 'POST',
-    url: authURL
-})
-// Axios
-.then(response=>console.log(response.data))
-.catch(error=>console.log(error))
-};
-
-
-
-// Game Search Data
-
-const dataURL = "https://api.igdb.com/v4/search";
-// let searchText = `search Call of Duty`;
-const searchData = {
-    url: dataURL,
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Client-ID': `${API_CLIENT_ID}`,
-        'Authorization': `Bearer ${API_AUTH}`},
-    data: 'fields id, name; search "Zelda"; limit 25;',
-};
-// console.log(authData);
-
-export const APIsearchGame = () => {
-axios(searchData)
-.then((res)=>{
-    // console.log(res.data);
-    let test = res.data;
-    console.log(test);
-
-})
-.catch(error=>console.log(error))
-
-};
-
-export const APIsearchScreenshot = () =>{
+  // Game Search Data
+  async findGame(searchtxt) {
+    let res;
     const searchData = {
-        url: "https://api.igdb.com/v4/screenshots",
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Client-ID': `${API_CLIENT_ID}`,
-            'Authorization': `Bearer ${API_AUTH}`},
-            data: "fields *; where game = 12018976;"
-        };
-    axios(searchData)
-  .then(response => {
-      console.log(response.data);
-  })
-  .catch(err => {
-      console.error(err);
-  });
+      method: "POST",
+      url: searchURL,
+      headers: {
+        Accept: "application/json",
+        "Client-ID": `${API_CLIENT_ID}`,
+        Authorization: `Bearer ${API_AUTH}`,
+      },
+      data: `fields id, name; search "${searchtxt}"; limit 100;`,
+    };
+    // console.log(searchData);
+    try {
+      res = await axios(searchData);
+      // console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+    return res.data;
+  },
 
+  // Search Image
+  async searchImage(gameID) {
+    let res;
+    const imageSearchData = {
+      url: imgURL,
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Client-ID": `${API_CLIENT_ID}`,
+        Authorization: `Bearer ${API_AUTH}`,
+      },
+      data: `fields *; where game = ${gameID};`,
+    };
+    try {
+      res = await axios(imageSearchData);
+    } catch (err) {
+      console.log(err);
+    }
+    return res.data;
+  },
 
+  // // Search all data from a game
+  async gameData(gameID) {
+    let res;
 
+      const searchData = {
+          url: dataURL,
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Client-ID': `${API_CLIENT_ID}`,
+              'Authorization': `Bearer ${API_AUTH}`},
+              data: `fields *; where id = ${gameID};`,
+          };
+
+          try {
+            res = await axios(searchData);
+          } catch (err) {
+            console.log(err);
+          }
+          return res.data;
+        },
 };
 
-
-
-
-
-
-
-
-
-  APIsearchScreenshot();
-
-// APIsearchGame();
-
-
+export default API;
