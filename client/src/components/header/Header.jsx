3e201850198cb5406f1c3f42aa52c13ccbc6485a
jcorozco81 <jcorozco@gmail.com>
2021-10-16 02:12:35 -0400

    Minor changes

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -9,7 +9,8 @@ export default function Header() {
       </div>
       <img
         className="headerImg"
-        src="https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/6/AmazonStores/ATVPDKIKX0DER/c56e1e493747e1bee78f78206aee8720.w1200.h400.png"
+        // src="https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/6/AmazonStores/ATVPDKIKX0DER/c56e1e493747e1bee78f78206aee8720.w1200.h400.png"
+        src="https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/best-dungeon-crawler-games1592591617374955.jpg"
         alt=""
       />
     </div>
