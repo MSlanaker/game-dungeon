a45ac19594f2bab5bdd98bbd5888373ee2d311d7
jcorozco81 <jcorozco@gmail.com>
2021-10-16 02:12:15 -0400

    Added blogs per game

--- client/src/pages/game/game.jsx
+++ client/src/pages/game/game.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-// import {Route} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import { useParams } from "react-router";
+import axios from "axios";
 
 function Game() {
   const [gameData, setgameData] = useState([]);
   const [coverImage, setcoverImage] = useState("");
+  const [posts, setPosts] = useState([]);
   const { id } = useParams();
   const apiURL = `/games/${id}`;
   const coverURL = `/games/cover/${id}`;
@@ -68,24 +70,33 @@ function Game() {
     renderCoverImage();
   }, []);
 
+
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(`/blogs/game/${id}`);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
+
+
+
+
   return (
     <div>
       <div class="card">
-        {/* <div class="card-image">
-    <figure>
-         class="image is-16by9">
-      <img src={coverImage.big} alt="Placeholder image"/>
-    </figure>
-  </div> */}
         <div class="card-content">
           <div class="media">
             <div class="media-left">
               <figure class="image is-512x512">
-                <img src={coverImage.big} alt="Placeholder image" />
+                <img src={coverImage.big} alt={gameData.name} />
               </figure>
             </div>
             <div class="media-content">
-              <p class="title is-4">{gameData.name}</p>
+              <p class="title is-2">{gameData.name}</p>
 
               <p class="subtitle is-6">
                 Game Status: {statusID[gameData.status]}
@@ -97,6 +108,34 @@ function Game() {
             </div>
           </div>
           <div class="content">
+          <p class="title is-4">Blogs about this game:{gameData.length}</p>
+
+
+          {posts.map((post) => {
+          return (
+            <div class="card">
+              <Route
+                render={({ history }) => (
+                    <div class="card-content">
+                    <p class="title is-5">{post.blogtitle}</p>
+                    <p class="subtitle is-6">By: {post.username}</p>
+
+                  <button
+
+                    class="button is-text"
+                    onClick={() => {
+                      history.push(`/post/${post._id}`);
+                    }}
+                  >Read Blog
+                  </button>
+                  </div>
+                )}
+              />
+            </div>
+          );
+        })}
+
+
             <br />
           </div>
         </div>
