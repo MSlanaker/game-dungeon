03dd48b8e74b4f7ddb9af9d71f327fbab925a90f
jcorozco81 <jcorozco@gmail.com>
2021-10-15 20:45:18 -0400

    Updated game route

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,7 @@ function App() {
         <Route path="/find/:id">
           <SearchResults />
         </Route>
-        <Route path="/game/">
+        <Route path="/game/:id">
           <Game />
         </Route>
       </Switch>
