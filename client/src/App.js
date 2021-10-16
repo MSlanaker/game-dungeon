100507988123a8ac87c3189c7ae9dd83e8a00d83
cjtrahey <trahey.03@gmail.com>
2021-10-16 01:33:22 -0400

    merging branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+<<<<<<< Updated upstream
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 
+=======
+import Topbar from "./components/topbar/TopBar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+>>>>>>> Stashed changes
 
 function App() {
   return (
--- client/src/components/singlePost/singlePost.jsx
+++ client/src/components/singlePost/singlePost.jsx
@@ -1,5 +1,6 @@
 import "./singlePost.css";
 
+<<<<<<< Updated upstream
 export default function SinglePost() {
     return (
         <div classname="singlePost">
@@ -27,6 +28,34 @@ export default function SinglePost() {
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores tempora modi reiciendis porro, dolore earum nemo consequatur tempore facilis ea facere doloremque cumque ad quaerat! Perspiciatis repudiandae vel corrupti porro.
                 </p>
             </div>
+=======
+export default function singlePost() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          alt=""
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor
+          <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span>
+            Author:
+            <b className="singlePostAuthor">
+              <Link className="link" to="/posts?username=user">
+                User
+              </Link>
+            </b>
+          </span>
+          <span>1 day ago</span>
+>>>>>>> Stashed changes
         </div>
     );
 }
